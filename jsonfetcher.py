549859a4a614d0b30823fddd796901c412831ffc
arpanetus <digitsaurum@gmail.com>
2017-11-20 04:07:47 +0600

    init commit

---
+++ jsonfetcher.py
@@ -0,0 +1,13 @@
+import requests
+
+url = "http://schedule.iitu.kz/rest/user/get_timetable_block.php"
+
+for i in range(13765, 14246):
+    querystring = {"block_id": str(i)}
+    response = requests.request("GET", url, params = querystring)
+    e = response.text.encode('utf-8')
+    with open(f"{i}.json", "ab") as f:
+        f.write(e)
+
+
+
